13cf4c4764444a944debcd0a3d231ebe2af1f3d3
Gali shalom <shalom.gali.47@gmail.com>
2026-05-28 14:03:37 +0300

    MapContext

--- MapContext.cpp
+++ MapContext.cpp
@@ -1,8 +1,9 @@
 #include "MapContext.h"
 
 // implement here your constructor and destructor
-
+MapContext::MapContext(IntermediateVec& threadVector) : _threadVector(threadVector){
+}
 void MapContext::addIntermediate(std::shared_ptr<K2> key, std::shared_ptr<V2> value)
 {
-    // TODO: implement this function!
+    _threadVector.emplace_back(std::move(key), std::move(value));
 }
--- MapContext.h
+++ MapContext.h
@@ -2,20 +2,27 @@
 #define MAP_CONTEXT_H
 
 #include "MapReduceKeys.h"
-// you can add other includes as you wish
+#include <vector>
+#include <utility>
+
 
 class MapContext
 {
 public:
     /*
+        Constructor that initialites the intermediate vector for each thread
+    */
+    explicit MapContext(IntermediateVec& threadVector);
+    /*
     You must keep and implement this function:
     */
     void addIntermediate(std::shared_ptr<K2> key, std::shared_ptr<V2> value);
 
-    /*
-    You can change everything else, including the constructor/desturctor
-    You can also add fields here (even public ones)
-    */
+    // Blocking the ability to copy instances of mapContext
+    MapContext(const MapContext&) = delete;
+    MapContext& operator=(const MapContext&) = delete;
+private:
+    IntermediateVec& _threadVector;
 };
 
 #endif // MAP_CONTEXT_H
